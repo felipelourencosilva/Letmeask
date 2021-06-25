8072a93e8f901e87ecafdb0a0b989429a074e282
lipelourencosilva <lipelourencosilva@gmail.com>
2021-06-25 17:49:19 -0300

    aula 05

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -2,7 +2,9 @@ import { FormEvent, useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
 
 import logoImg from "../assets/images/logo.svg";
-import deleteImg from '../assets/images/delete.svg';
+import deleteImg from "../assets/images/delete.svg";
+import checkImg from "../assets/images/check.svg";
+import answerImg from "../assets/images/answer.svg";
 
 import { RoomCode } from "../components/RoomCode";
 import { Button } from "../components/Button";
@@ -19,25 +21,37 @@ type RoomParams = {
 };
 
 export function AdminRoom() {
-  // const { user } = useAuth(); 
+  // const { user } = useAuth();
   const params = useParams<RoomParams>();
   const roomId = params.id;
   const history = useHistory();
-  
+
   const { title, questions } = useRoom(roomId);
 
   async function handleEndRoom() {
     await database.ref(`rooms/${roomId}`).update({
       endedAt: new Date(),
-    })
+    });
 
-    history.push('/')
+    history.push("/");
   }
 
   async function handleDeleteQuestion(questionId: string) {
-    if(window.confirm("Tem certeza que você deseja excluir esta pergunta?")) {
-      const questionRef = await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    if (window.confirm("Tem certeza que você deseja excluir esta pergunta?")) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
     }
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
   }
 
   return (
@@ -47,7 +61,9 @@ export function AdminRoom() {
           <img src={logoImg} alt="Letmeask" />
           <div>
             <RoomCode code={roomId} />
-            <Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
+            <Button isOutlined onClick={handleEndRoom}>
+              Encerrar sala
+            </Button>
           </div>
         </div>
       </header>
@@ -61,11 +77,30 @@ export function AdminRoom() {
         <div className="question-list">
           {questions.map((question) => {
             return (
-              <Question 
+              <Question
                 key={question.id}
-                content={question.content} 
-                author={question.author} 
+                content={question.content}
+                author={question.author}
+                isAnswered={question.isAnswered}
+                isHighlighted={question.isHighlighted}
               >
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img src={checkImg} alt="Marcar pergunta como respondida"/>
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleHighlightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque a pergunta" />
+                    </button>
+                  </>
+                )}
+
                 <button
                   type="button"
                   onClick={() => handleDeleteQuestion(question.id)}
